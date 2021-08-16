1bb44ca77ab16b54e1ef66c1d8876ebf8a16faa6
parsasharify <79376827+parsasharify@users.noreply.github.com>
2021-08-16 01:43:42 -0700

    Add files via upload

---
+++ WebApplication1/WebApplication1/Controllers/SimpleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class SimpleController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            Console.WriteLine("ASD");
+            return "Hello world!";
+        }
+    }
+}
